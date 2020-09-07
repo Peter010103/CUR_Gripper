acfbdac01012be655379cd47caf712fb1297934e
Peter010103 <peter62233460@gmail.com>
2020-09-08 04:20:26 +0900

    Added catkin workspace // Need to fix problem with 4 bar linkages and RViz

---
+++ catkin_ws/build/GripperV4_URDF_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peterwoo/catkin_ws/src/GripperV4_URDF_description
+# Build directory: /home/peterwoo/catkin_ws/build/GripperV4_URDF_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/gripper_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peterwoo/catkin_ws/src/gripper_description
+# Build directory: /home/peterwoo/catkin_ws/build/gripper_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
